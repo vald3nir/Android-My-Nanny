45c37d3b283869e537cce00c444f3fdc70d284c5
vald3nir <vald3nir@gmail.com>
2022-01-03 17:14:17 -0300

    remove server code

--- Server-My-Nanny/src/utils/translator_utils.py
+++
@@ -1,7 +0,0 @@
-from googletrans import Translator
-
-
-def translate_string(message, language='pt-br'):
-    translator = Translator()
-    result = translator.translate(message, src='en', dest=language)
-    return result.text
