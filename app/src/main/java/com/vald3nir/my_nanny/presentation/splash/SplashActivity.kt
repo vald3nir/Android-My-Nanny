048cf32705f62681ba9a663979b5b3bbfdc94440
Valdenir Severino <vald3nir@gmail.com>
2022-02-09 09:16:31 -0300

    finish splash screen

--- app/src/main/java/com/vald3nir/my_nanny/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/vald3nir/my_nanny/presentation/splash/SplashActivity.kt
@@ -1,10 +1,8 @@
 package com.vald3nir.my_nanny.presentation.splash
 
-import android.content.Intent
 import android.os.Bundle
 import com.vald3nir.my_nanny.common.core.BaseActivity
 import com.vald3nir.my_nanny.databinding.ActivitySplashBinding
-import com.vald3nir.my_nanny.presentation.login.LoginActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SplashActivity : BaseActivity() {
@@ -16,16 +14,13 @@ class SplashActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        viewModel.view = this
         initViews()
     }
 
     private fun initViews() {
-//        viewModel.setView(this)
-        binding.apply {
-            loading.showLoadingAnimation()
-        }
-        startActivity(Intent(this, LoginActivity::class.java))
+        binding.loading.showLoadingAnimation()
+        viewModel.checkUserLogger()
     }
-
 
 }
--- app/src/main/java/com/vald3nir/my_nanny/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/vald3nir/my_nanny/presentation/splash/SplashViewModel.kt
@@ -1,10 +1,26 @@
 package com.vald3nir.my_nanny.presentation.splash
 
+import androidx.lifecycle.viewModelScope
 import com.vald3nir.my_nanny.common.core.BaseViewModel
+import com.vald3nir.my_nanny.domain.AppNavigation
+import com.vald3nir.my_nanny.domain.AuthUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class SplashViewModel : BaseViewModel() {
+class SplashViewModel(
+    private val appNavigation: AppNavigation,
+    private val authUseCase: AuthUseCase,
+) : BaseViewModel() {
 
-
-
-
+    fun checkUserLogger() {
+        viewModelScope.launch(Dispatchers.IO) {
+            authUseCase.checkUserLogger(view,
+                onSuccess = {
+                    appNavigation.redirectToDashboard(view)
+                }, onError = {
+                    appNavigation.redirectToLogin(view)
+                }
+            )
+        }
+    }
 }
